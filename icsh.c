f2fbb11492256848bf92e8e0f451296e81d570db
Weilong Xu <edison@Edisons-MacBook-Air.local>
2025-05-29 02:34:01 +0700

    0.1.0

--- icsh.c
+++ icsh.c
@@ -1,17 +1,47 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Weilong Xu
+ * StudentID: 6581036
  */
 
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_CMD_BUFFER 255
 
 int main() {
+    printf("Starting IC shell\n");
+    printf("Welcome to ICSH by Edison\n");
+
     char buffer[MAX_CMD_BUFFER];
+    char preBuffer[MAX_CMD_BUFFER] = "";
+
     while (1) {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+        if (strlen(buffer) == 1) {
+            continue;
+        }
+        else if (strncmp(buffer, "echo ", 5) == 0) {
+            char *strToPrint = buffer + 5;
+            printf("%s", strToPrint);
+        }
+        else if (strncmp(buffer, "!!", 2) == 0 && strlen(buffer) == 3) {
+            if (strncmp(preBuffer, "echo ", 5) == 0) {
+                char *strToPrint = preBuffer + 5;
+                printf("%s", preBuffer);
+                printf("%s", strToPrint);
+                continue;
+            }
+        }
+        else if (strncmp(buffer, "exit ", 5) == 0 && strlen(buffer) >= 7) {
+            int code = atoi(buffer + 5) & 0xFF;
+            printf("bye\n");
+            exit(code);
+        }
+        else {
+            printf("bad command\n");
+        }
+        strcpy(preBuffer, buffer);
     }
 }
